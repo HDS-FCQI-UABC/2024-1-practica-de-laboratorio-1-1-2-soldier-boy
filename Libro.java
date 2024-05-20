dbf73eda09c3ca09aa6696812d68a714d0211831
HuangWenxuan <huang.wenxuan@uabc.edu.mx>
2024-05-20 11:33:37 -0700

    Se creo el proyecto y se agrego las clases.

---
+++ Libro.java
@@ -0,0 +1,10 @@
+public class Libro {
+	private String titulo;
+	private String autor;
+	private String fecha_publicacion;
+	private String genero;
+
+	public void obtenerInformacion() {
+		throw new UnsupportedOperationException();
+	}
+}
